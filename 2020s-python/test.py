be8dd676a9ba2270af40caa015e4d1f76078096d
dynle <doyun04180@gmail.com>
2020-07-16 02:32:43 +0900

    update

--- 2020s-python/test.py
+++ 2020s-python/test.py
@@ -1,15 +1,21 @@
 from multiprocessing import Process, Queue
-import logging
+import time
+# import logging
 
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%I:%M:%S', level=logging.INFO)
+# logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%I:%M:%S', level=logging.INFO)
 
-def f(q):
-    q.put([42, None, 'hello'])
+# def f(q):
+#     q.put([42, None, 'hello'])
 
-if __name__ == '__main__':
-    q = Queue()
-    print(q)
-    p = Process(target=f, args=(q,))
-    p.start()
-    logging.info("result :{}".format(q.get()))
-    p.join()
+# if __name__ == '__main__':
+#     q = Queue()
+#     print(q)
+#     p = Process(target=f, args=(q,))
+#     p.start()
+#     logging.info("result :{}".format(q.get()))
+#     p.join()
+
+begin = time.time()
+time.sleep(5)
+end = time.time()
+print('실행 시간: {0:.3f}초'.format(end - begin))
